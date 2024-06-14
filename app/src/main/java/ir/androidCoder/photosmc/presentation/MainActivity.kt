f69c4d49c8df037ccb818a5efd2b93f53a513cc3
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-13 18:34:30 -1200

    Setup Navigation Compose

--- app/src/main/java/ir/androidCoder/photosmc/presentation/MainActivity.kt
+++ app/src/main/java/ir/androidCoder/photosmc/presentation/MainActivity.kt
@@ -3,45 +3,62 @@ package ir.androidCoder.photosmc.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import ir.androidCoder.photosmc.presentation.feature.PhotoScreen
 import ir.androidCoder.photosmc.presentation.ui.theme.PhotosMCTheme
+import ir.androidCoder.photosmc.presentation.viewModel.MainViewModel
+import ir.androidCoder.photosmc.util.MyScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             PhotosMCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                
+                MainUi(viewModel = viewModel)
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainUi(viewModel: MainViewModel) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhotosMCTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        NavScreen(viewModel)
+
     }
 }
+
+@Composable
+fun NavScreen(viewModel: MainViewModel) {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = MyScreen.PhotoScreen.route){
+
+        composable(MyScreen.PhotoScreen.route){
+            PhotoScreen(viewModel)
+        }
+
+    }
+
+}
+
